6160d5b86eaf05960970f0b52b8e7ccd77249e1f
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-01-08 16:34:18 +0100

    fix style and add js comments

--- script/script.js
+++ script/script.js
@@ -1,16 +1,6 @@
-// esercizio di oggi: Email List
-// nome repo: js-email-list
-// Descrizione
-// Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
-// Doc Axios: https://axios-http.com/
-// Bonus
-// Abbellire con CSS o Bootstrap
-// Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
-// Buon Lavoro
-
-
-
 //FUNCTION
+
+//funzione che genera una lista di 10 email tramite api boolean ed un ciclo for
 function createList(){
     for(let i = 1; i <= 10; i++){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
@@ -22,6 +12,7 @@ function createList(){
     }
 }
 
+//funzione che elimina dall'HTML la vecchia lista e ne crea una nuova utilizzando la funzione createList()
 function createNewList(){
     ListElm.innerHTML = ``
     createList()
@@ -32,5 +23,9 @@ const ListElm = document.getElementById("mailList")
 const BtnElm = document.getElementById("btn")
 
 //EVENT
+
+//richiamo della funzione createList() per la creazione della lista di email iniziale
 createList()
+
+//richiamo della funzione createNewList() al click del bottone per generare una nuova lista di email
 BtnElm.addEventListener("click", createNewList)
